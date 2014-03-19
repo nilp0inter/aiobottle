36004343fc09017c3a88012204f7631db4d403af
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-03-19 01:33:09 +0100

    Create task instead of run the loop in AsyncServer.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from bottle import Bottle, ServerAdapter, response, request, HTTPResponse\
-        , HTTPError, tob, _e, html_escape, DEBUG, RouteReset
+from bottle import Bottle, ServerAdapter, response, request, HTTPResponse
+from bottle import HTTPError, tob, _e, html_escape, DEBUG, RouteReset
 from traceback import format_exc
 
 import sys
@@ -20,29 +20,29 @@ ch = logging.StreamHandler()
 ch.setFormatter(formater)
 logger.addHandler(ch)
 
+
 class AsyncServer(ServerAdapter):
     def run(self, handler):
         def wsgi_app(env, start):
             def start_response(status_line, headerlist, exc_info=None):
                 status_code = status_line.split(' ', 1)[0]
-                headerdict = dict(map(lambda x: (x[0].lower(), x[1]), headerlist))
+                headerdict = dict(map(lambda x: (x[0].lower(), x[1]),
+                                      headerlist))
                 length = headerdict.get('content-length', 0)
-                logger.error('{} {} {} {}'.format(env['REQUEST_METHOD'],
+                logger.error('{} {} {} {}'.format(
+                    env['REQUEST_METHOD'],
                     env['RAW_URI'], status_code, length))
                 return start(status_line, headerlist, exc_info)
             return handler(env, start_response)
         loop = asyncio.get_event_loop()
         f = loop.create_server(
-                lambda: WSGIServerHttpProtocol(wsgi_app, loop = loop, readpayload=True),
-                self.host, self.port)
-        loop.run_until_complete(f)
-        try:
-            loop.run_forever()
-        except KeyboardInterrupt:
-            pass
+            lambda: WSGIServerHttpProtocol(wsgi_app, loop=loop,
+                                           readpayload=True),
+            self.host, self.port)
+        asyncio.Task(f)
+
 
 class AsyncBottle(Bottle):
-
     def _handle(self, environ):
         try:
             environ['bottle.app'] = self
@@ -64,7 +64,8 @@ class AsyncBottle(Bottle):
         except (KeyboardInterrupt, SystemExit, MemoryError):
             raise
         except Exception:
-            if not self.catchall: raise
+            if not self.catchall:
+                raise
             stacktrace = format_exc()
             environ['wsgi.errors'].write(stacktrace)
             return HTTPError(500, "Internal Server Error", _e(), stacktrace)
@@ -75,15 +76,17 @@ class AsyncBottle(Bottle):
             out = self._cast((yield from self._handle(environ)))
             # rfc2616 section 4.3
             if response._status_code in (100, 101, 204, 304)\
-            or environ['REQUEST_METHOD'] == 'HEAD':
-                if hasattr(out, 'close'): out.close()
+                    or environ['REQUEST_METHOD'] == 'HEAD':
+                if hasattr(out, 'close'):
+                    out.close()
                 out = []
             start_response(response._status_line, response.headerlist)
             return out
         except (KeyboardInterrupt, SystemExit, MemoryError):
             raise
         except Exception:
-            if not self.catchall: raise
+            if not self.catchall:
+                raise
             err = '<h1>Critical error while processing request: %s</h1>' \
                   % html_escape(environ.get('PATH_INFO', '/'))
             if DEBUG:
@@ -92,7 +95,8 @@ class AsyncBottle(Bottle):
                        % (html_escape(repr(_e())), html_escape(format_exc()))
             environ['wsgi.errors'].write(err)
             headers = [('Content-Type', 'text/html; charset=UTF-8')]
-            start_response('500 INTERNAL SERVER ERROR', headers, sys.exc_info())
+            start_response('500 INTERNAL SERVER ERROR', headers,
+                           sys.exc_info())
             return [tob(err)]
 
     def __call__(self, environ, start_response):
